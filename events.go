2fa1b42613449e24db2d1e627e36c1b4ad4e4793
turnipsoup <jeremy@jeremyheckt.com>
2022-05-18 18:56:19 -0700

    Gave it an ending, essentially. Its a thing now!

--- events.go
+++ events.go
@@ -58,6 +58,21 @@ func updateLocationItemVisibility(g *Game, loc string, item string, val bool) {
 	}
 }
 
+func updateLocationItemDescription(g *Game, loc string, item string, val string) {
+	for l := range g.locations {
+		if loc == g.locations[l].Name {
+			for i := range g.locations[l].Objects {
+				if g.locations[l].Objects[i].Name == item {
+					g.locations[l].Objects[i].Details = val
+					return
+				}
+
+			}
+
+		}
+	}
+}
+
 /////
 
 // If something has scripting associated with examining it, put it in here
@@ -69,6 +84,7 @@ func handleExamineScripting(g *Game, cmdArgs []string) {
 		updateLocationDescription(g, "hallway", 1)
 		updateLocationDescription(g, "kitchen", 1)
 		updateConnectedLocationVisibility(g, "kitchen", "pantry", true)
+		updateLocationItemVisibility(g, "hallway", "family_photo", true)
 	}
 
 	// Picking up the keys in the pantry
@@ -76,6 +92,17 @@ func handleExamineScripting(g *Game, cmdArgs []string) {
 		updateLocationDescription(g, "parlor", 1)
 		updateConnectedLocationVisibility(g, "parlor", "courtyard", true)
 		updateLocationItemVisibility(g, "pantry", "keys", false)
+	}
+
+	// Picking up the journal in the quarters
+	if exobj == "journal" && g.currentLocation == "quarters" {
+		updateLocationItemVisibility(g, "quarters", "glasses", true)
+		updateLocationItemDescription(g, "cave_entrance", "blue_book", "'The time has come. We have waited patiently for over a decade. We have suffered through his annoying happiness and jarring wholesome attitude. Our spawn is now 10 years old - old enough for the ritual to take place. 10 years I have dealt with this annoying man, and now I shall transfer his mind into our spawn to create a genius. If everything works then he should remain here in a vegetative state forever. The only one who can undo this spell is the spawn itself on its own free will, awakening him.'")
+	}
+
+	// Picking up the glasses in the quarters
+	if exobj == "glasses" && g.currentLocation == "quarters" {
+		updateLocationItemDescription(g, "cave_entrance", "blue_book", "'The time has come. We have waited patiently for over a decade. We have suffered through his annoying happiness and jarring wholesome attitude. Our spawn is now 10 years old - old enough for the ritual to take place. 10 years I have dealt with this annoying man, and now I shall transfer his mind into our spawn to create a genius. If everything works then he should remain here in a vegetative state forever. The only one who can undo this spell is the spawn itself on its own free will, awakening him.'")
 	}
 }
 
